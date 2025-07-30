cad802a19b239e7cdda57b073ec29da08c53ffc7
Kristen Won <kjwon@usc.edu>
2025-07-29 17:09:01 -0700

    fonts

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,16 +1,19 @@
 'use client';
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Header() {
     return (
         <div className="w-full h-20 bg-[#0C0C0C] flex items-center justify-between px-20 border-b border-[#353535]">
             <div className="flex flex-row pl-6 gap-x-3">
-                <Image
-                    src="/shepherd-name.png"
-                    height={70}
-                    width={140}
-                    alt="Shepherd Logo"
-                />
+                <Link href="/">
+                    <Image
+                        src="/shepherd-name.png"
+                        height={70}
+                        width={140}
+                        alt="Shepherd Logo"
+                    />
+                </Link>
             </div>
             <div className="flex flex-row space-x-2 items-center">
                 <Image
