cab978fc604ca3dcb80bcdc7dd8e2b6bcb566a44
Kristen Won <kjwon@usc.edu>
2025-08-04 01:00:58 -0700

    fixed cors pt 2

--- backend/app/main.py
+++ backend/app/main.py
@@ -38,7 +38,7 @@ from fastapi.middleware.cors import CORSMiddleware
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://shepherd-mvp.vercel.app/"],  # or specify your frontend origin like ["http://localhost:3000"]
+    allow_origins=["https://shepherd-mvp.vercel.app"],  # or specify your frontend origin like ["http://localhost:3000"]
     allow_credentials=True,
     allow_methods=["*"],  # <- this is important
     allow_headers=["*"],
