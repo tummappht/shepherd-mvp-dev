59099bc3fbfbc64befadca730351aafd3eabb7d1
tummappht <p.ongartitthichai@gmail.com>
2025-10-27 16:18:29 +0700

    feat: add service to update user session name

--- frontend/src/services/user.js
+++ frontend/src/services/user.js
@@ -1,4 +1,4 @@
-import { get } from "./utils";
+import { get, patch } from "./utils";
 
 export const serviceUserSessions = async (params) => {
   return get(`/user/sessions`, { params });
@@ -6,4 +6,8 @@ export const serviceUserSessions = async (params) => {
 
 export const serviceUserSessionByRunId = async (runId) => {
   return get(`/user/sessions/${runId}`);
+};
+
+export const serviceUpdateUserSessionName = async (runId, name) => {
+  return patch(`/user/sessions/${runId}/name`, { name });
 };
