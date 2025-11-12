1434d28f0523487e6c3805a1b45a1045958b4da5
tummappht <p.ongartitthichai@gmail.com>
2025-11-12 10:43:23 +0700

    feat: add Layout component for Create User page with metadata and styling

---
+++ frontend/src/app/admin/create-user/layout.js
@@ -0,0 +1,15 @@
+import "../globals.css";
+import PropTypes from "prop-types";
+import StaticLayout from "@/components/layouts/StaticLayout";
+
+export const metadata = {
+  title: "Shepherd Security: Create User",
+};
+
+export default function Layout({ children }) {
+  return <StaticLayout isSidebar={false}>{children}</StaticLayout>;
+}
+
+Layout.propTypes = {
+  children: PropTypes.node.isRequired,
+};
